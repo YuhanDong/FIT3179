e0bd83a436d067ffc6d49fe1eaa361966bc07e38
Yuhan Dong <60382478+YuhanDong@users.noreply.github.com>
2022-09-26 20:29:45 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,5 @@
 var vg_1 = "assignment2_chart1.vg.json";
 vegaEmbed("#choropleth", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// Access the Vega view instance 
+  (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
